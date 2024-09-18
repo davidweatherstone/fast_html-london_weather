16e54389a4ff9ac6eb83e85e4911aecc583d734a
David Weatherstone <djweatherstone@googlemail.com>
2024-09-18 09:20:32 +0100

    Multipage app

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
-from fasthtml.common import Style, Link, fast_app, serve, Title, Main, Body, Header, H1, Input, Form, P, Div, Select, Option
+import json
+
+from fasthtml.common import Style, Link, fast_app, serve, Title, Main, Body, Header, H1, Input, Form, P, Div, Select, Option, Script, Ul, Li, A
+
+from fh_altair import altair_headers, altair2fasthtml
 from fh_matplotlib import matplotlib2fasthtml
+
+import altair as alt
 import matplotlib.pyplot as plt
+
 import polars as pl
 
 style = Link(rel="stylesheet", href="assets/general.css", type="text/css")
@@ -9,29 +16,54 @@ body_style = """
           padding: 0; margin: 0; box-sizing: border-box; font-size: 62.5%; 
           font-family: 'Rubik', sans-serif; line-height: 1; font-weight: 400; color: #555
           """
+plot_lib = Script(src="https://cdn.plot.ly/plotly-2.32.0.min.js")
 
 # Start FastHTML app
-app, rt = fast_app(pico=False, hdrs=(style, font))
+app, rt = fast_app(pico=False, hdrs=(style, font, altair_headers, plot_lib))
 
 dataset_source = r"london_weather.csv"
 
 df = pl.read_csv(dataset_source)
 
-tweaked_df = (df
-.with_columns(
-  year=pl.col("date").cast(pl.String).str.to_datetime("%Y%m%d").dt.year())
-.group_by('year', maintain_order=True)
-.agg(pl.col(pl.Float64).mean())
-)
+def tweak_dataframe(df):
+    tweaked_df = (df
+          .with_columns(
+            year=pl.col("date").cast(pl.String).str.to_datetime("%Y%m%d").dt.year())
+          .group_by('year', maintain_order=True)
+          .agg(pl.col(pl.Float64).mean())
+          )
+    return tweaked_df
 
-cols = tweaked_df.columns[1:]
+tweaked_df = tweak_dataframe(df)
 
 # Initialize with default values
 selected_column = tweaked_df.columns[1]
 selected_year = tweaked_df['year'].min()
+cols = tweaked_df.columns[1:]
+
+# Plotting functions
+def altair_weather_plot(data, column):
+   col_name = str(column).replace("_", " ").title()
+   title = f'Scatter Plot of Mean {col_name} by Year'
+   color="#0b2545"
+
+   chart = alt.Chart(data, title=title).mark_point(color=color).encode(
+        alt.X("year:O").title("Year"),
+        alt.Y(f"{column}:Q").title(col_name)
+        ).properties(width=400, height=200
+        ).add_params(alt.selection_interval())
+   
+   bars = alt.Chart(data).mark_bar(color=color).encode(
+        alt.Y("year:O").title("Year"),
+        alt.X(f"{column}:Q").title(col_name)
+        ).properties(width=400, height=200
+        ).add_params(alt.selection_interval())
+   
+   return altair2fasthtml(chart & bars)
+
 
 @matplotlib2fasthtml
-def weather_plot(data, column):
+def mpl_weather_plot(data, column):
   color = "#0b2545"
   fig, ax = plt.subplots()
   ax.scatter(data['year'], data[column], s=60, color=color)
@@ -51,10 +83,38 @@ def weather_plot(data, column):
   ax.spines['left'].set_color(color)
 
 
+def plotly_weather_plot(data, column):
+   years = data['year'].to_list()
+   values = data[column].to_list()  # This should be the y-axis value
+   plotly_data = [{
+      "x": years,
+      "y": values,
+      "type": "scatter",
+      "mode": "markers",
+      "name": f'Scatter Plot of Mean {str(column).replace("_", " ").title()} by Year'
+   }]
+   
+   return json.dumps(plotly_data)
+
+
 @app.get("/")
 def get():
+   return Main(
+       H1("Select report", cls="header"),
+       Ul(
+           Li(A("Altair", href="/altair"), cls="list-item link"),
+           Li(A("Matplotlib", href="/matplotlib"), cls="list-item link"),
+           Li(A("Plotly", href="/plotly"), cls="list-item link"),
+           cls="list"
+       ), cls="container"
+   )
+
+# Separate pages
+# Altair
+@app.get("/altair")
+def altair():
   global selected_year
-  plot = weather_plot(tweaked_df, selected_column)
+  plot = altair_weather_plot(tweaked_df, selected_column)
   column_select = Select(*[Option(str(col).replace("_", " ").title(), value=col) for col in cols], 
                          name="column_btns", form="filter-form")
 
@@ -64,12 +124,12 @@ def get():
                      max=tweaked_df['year'].max(),
                      value=selected_year)
   
-  return Title("London Weather Data"), Main(
+  return Title("London Weather Data, Altair"), Main(
     Body(
-
       Div(
           Div(
               Form(
+                  A("Navigate Home", href="/", cls="back"),
                   P("Filters", 
                   cls="heading-tertiary"),
                   P("Select column", 
@@ -79,13 +139,13 @@ def get():
                   cls="subheading"),
                   Div(year_slider),
                   P(selected_year, cls="heading-tertiary", id="selected-year"),
-                  id="filter-form", hx_trigger="input", hx_post="/update_filters", hx_target="#chart", hx_swap="innerHTML",
+                  id="filter-form", hx_trigger="input", hx_post="/update_altair_filters", hx_target="#chart", hx_swap="innerHTML",
               cls="filter-pane"
               )
           ),
           Div(
             Header(
-              H1("London Weather Data", cls="heading-secondary"),
+              H1("London Weather Data, Altair", cls="heading-secondary"),
               cls="header"
               ),
             plot,
@@ -98,8 +158,8 @@ def get():
   )
       
 
-@app.post("/update_filters")
-def update_filters(data: dict):
+@app.post("/update_altair_filters")
+def update_altair_filters(data: dict):
     global selected_column, selected_year
 
     selected_column = data["column_btns"]
@@ -108,18 +168,165 @@ def update_filters(data: dict):
     filtered_df = tweaked_df.filter(pl.col("year") >= selected_year)
 
     # Create updated plot
-    updated_plot = weather_plot(filtered_df, selected_column)
+    updated_plot = altair_weather_plot(filtered_df, selected_column)
+    
+    return Div(
+              Div(
+                Header(
+                  H1("London Weather Data, Altair", cls="heading-secondary"),
+                  cls="header"
+                  ),
+                updated_plot,
+                id="chart"
+              ),
+              P(selected_year, cls="heading-tertiary", id="selected-year", hx_swap_oob="true")
+    )
+
+# MPL
+@app.get("/matplotlib")
+def matplotlib():
+  global selected_year
+  plot = mpl_weather_plot(tweaked_df, selected_column)
+  column_select = Select(*[Option(str(col).replace("_", " ").title(), value=col) for col in cols], 
+                         name="column_btns", form="filter-form")
+
+  year_slider = Input(type="range",
+                     name="year_range",
+                     min=tweaked_df['year'].min(),
+                     max=tweaked_df['year'].max(),
+                     value=selected_year)
+  
+  return Title("London Weather Data, Matplotlib"), Main(
+    Body(
+
+      Div(
+          Div(
+              Form(
+                  A("Navigate Home", href="/", cls="back"),
+                  P("Filters", 
+                  cls="heading-tertiary"),
+                  P("Select column", 
+                  cls="subheading"),
+                  Div(column_select),
+                  P("Select Start Year", 
+                  cls="subheading"),
+                  Div(year_slider),
+                  P(selected_year, cls="heading-tertiary", id="selected-year"),
+                  id="filter-form", hx_trigger="input", hx_post="/update_mpl_filters", hx_target="#chart", hx_swap="innerHTML",
+              cls="filter-pane"
+              )
+          ),
+          Div(
+            Header(
+              H1("London Weather Data, Matplotlib", cls="heading-secondary"),
+              cls="header"
+              ),
+            plot,
+            id="chart"
+          ),
+          cls="container grid--2-cols-layout"
+      ),
+      style=body_style
+    )
+  )
+      
+
+@app.post("/update_mpl_filters")
+def update_mpl_filters(data: dict):
+    global selected_column, selected_year
+
+    selected_column = data["column_btns"]
+    selected_year = int(data["year_range"])
+
+    filtered_df = tweaked_df.filter(pl.col("year") >= selected_year)
+
+    # Create updated plot
+    updated_plot = mpl_weather_plot(filtered_df, selected_column)
     
     return Div(
               Div(
                     Header(
-                      H1("London Weather Data", cls="heading-secondary"),
+                      H1("London Weather Data, Matplotlib", cls="heading-secondary"),
                       cls="header"
                       ),
                     updated_plot,
                     id="chart",
                     cls=""
                   ),
+              P(selected_year, cls="heading-tertiary", id="selected-year", hx_swap_oob="true")
+    )
+
+# Plotly
+@app.get("/plotly")
+def plotly():
+  global selected_year
+  plot = plotly_weather_plot(tweaked_df, selected_column)
+  column_select = Select(*[Option(str(col).replace("_", " ").title(), value=col) for col in cols], 
+                         name="column_btns", form="filter-form")
+
+  year_slider = Input(type="range",
+                     name="year_range",
+                     min=tweaked_df['year'].min(),
+                     max=tweaked_df['year'].max(),
+                     value=selected_year)
+  
+  return Title("London Weather Data, Plotly"), Main(
+    Body(
+      Div(
+          Div(
+              Form(
+                  A("Navigate Home", href="/", cls="back"),
+                  P("Filters", 
+                  cls="heading-tertiary"),
+                  P("Select column", 
+                  cls="subheading"),
+                  Div(column_select),
+                  P("Select Start Year", 
+                  cls="subheading"),
+                  Div(year_slider),
+                  P(selected_year, cls="heading-tertiary", id="selected-year"),
+                  id="filter-form", hx_trigger="input", hx_post="/update_plotly_filters", hx_target="#chart", hx_swap="innerHTML",
+              cls="filter-pane"
+              )
+          ),
+          Div(
+            Header(
+              H1("London Weather Data, Plotly", cls="heading-secondary"),
+              cls="header"
+              ),
+            Div(id="myDiv"),  # Chart will be rendered here
+            Script(f"var data = {plot}; Plotly.newPlot('myDiv', data);"),
+            id="chart"
+          ),
+          cls="container grid--2-cols-layout"
+      ),
+      style=body_style
+    )
+  )
+      
+
+@app.post("/update_plotly_filters")
+def update_plotly_filters(data: dict):
+    global selected_column, selected_year
+
+    selected_column = data["column_btns"]
+    selected_year = int(data["year_range"])
+
+    filtered_df = tweaked_df.filter(pl.col("year") >= selected_year)
+
+    # Create updated plot
+    updated_plot = plotly_weather_plot(filtered_df, selected_column)
+    
+    return Div(
+              Div(
+                Header(
+                  H1("London Weather Data, Plotly", cls="heading-secondary"),
+                  cls="header"
+                  ),
+                Div(id="myDiv"),  # Chart will be rendered here
+                Script(f"var data = {updated_plot}; Plotly.newPlot('myDiv', data);"),
+                id="chart"
+              ),
               P(selected_year, cls="heading-tertiary", id="selected-year", hx_swap_oob="true")
     )
 
